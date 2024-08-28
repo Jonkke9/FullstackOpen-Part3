c56c8a42b91a9598b8520e5c61a79cfa4e0cf111
Jonkke9 <joonas.ka.julkunen@gmail.com>
2024-08-29 02:30:32 +0300

    finished 3.6

--- index.js
+++ index.js
@@ -84,6 +84,12 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
+  if (persons.some(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
   const person = {
     name: body.name,
     number: body.number,
